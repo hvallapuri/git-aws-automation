2162b77664cf8d3321d503e31b1eb9a120777991
hvallapuri <vallapurihde@gmail.com>
2021-12-28 21:20:39 +0000

    Version 2

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 def handler(event, context):
-    data = {'column1': [10, 15], 'column2': [8, 7]}
+    data = {'column1': [10.8, 15.9], 'column2': [8, 7]}
     df = pd.DataFrame(data)
     print(df)
+    print("Processing completed")
